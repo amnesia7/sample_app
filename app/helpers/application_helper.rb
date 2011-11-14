d4099eccba5a938b9d33b2ad371328e80b8bf6e4
Colin Gibson <col.gibson@gmail.com>
2011-11-14 19:50:17 +0000

    Finished first cut of the User model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,10 @@ module ApplicationHelper
 			"#{base_title} | #{@title}"
 		end
 	end
+
+	# return the website logo
+	def logo
+		image_tag("logo.png", :alt => "Sample App", :class => "round")
+	end
+
 end
