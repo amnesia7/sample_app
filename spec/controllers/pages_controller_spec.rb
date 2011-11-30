526750c134f255c3b3c64ac8e3aba8ce2090b46e
Colin Gibson <col.gibson@gmail.com>
2011-11-30 11:55:23 +0000

    Added user following

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -9,29 +9,48 @@ describe PagesController do
 
   describe "GET 'home'" do
 
-    before(:each) do
-      @user = Factory(:user)
-      test_sign_in(@user)
+    describe "when not signed in" do
+
+      before(:each) do
+        get :home
+      end
+
+      it "should be successful" do
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        response.should have_selector("title",
+                                      :content => @base_title + " | Home")
+      end
+
     end
 
-    it "should be successful" do
-      get :home
-      response.should be_success
-    end
+    describe "when signed in" do
 
-    it "should have the right title" do
-      get :home
-      response.should have_selector("title",
-                                    :content => @base_title + " | Home")
-    end
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        other_user = Factory(:user, :email => Factory.next(:email))
+        other_user.follow!(@user)
+      end
 
-    it "should display correctly pluralize 'micropost'" do
-      mp1 = Factory(:micropost, :user => @user, :content => "Foo bar")
-      get :home
-      response.should have_selector("span.microposts", :content => "1 micropost")
-      mp2 = Factory(:micropost, :user => @user, :content => "Bar faz")
-      get :home
-      response.should have_selector("span.microposts", :content => "2 microposts")
+      it "should have the right follower/following counts" do
+        get :home
+        response.should have_selector("a", :href => following_user_path(@user),
+                                           :content => "0 following")
+        response.should have_selector("a", :href => followers_user_path(@user),
+                                           :content => "1 follower")
+      end
+
+      it "should display correctly pluralize 'micropost'" do
+        mp1 = Factory(:micropost, :user => @user, :content => "Foo bar")
+        get :home
+        response.should have_selector("span.microposts", :content => "1 micropost")
+        mp2 = Factory(:micropost, :user => @user, :content => "Bar faz")
+        get :home
+        response.should have_selector("span.microposts", :content => "2 microposts")
+      end
+
     end
 
   end
